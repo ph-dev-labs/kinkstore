737643361a000c8a734d24d212977fd373eab3cb
ph-dev-labs <85322356+ph-dev-labs@users.noreply.github.com>
2023-11-30 10:24:29 +0000

    https

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://realtorecom.citimal.com/api",
+  baseUrl: "https://realtorecom.citimal.com/api",
 });
 
 
