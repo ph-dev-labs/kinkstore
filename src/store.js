495f6ce28199f40fcf0ffa41adf61e96e276efb1
nwosu chimezie <85322356+ph-dev-labs@users.noreply.github.com>
2023-12-13 10:43:33 +0000

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { kinkApi } from "./redux/api/api";
-import cartReducer from "./redux/Cart/Cart";
+import cartReducer, {initializeCart} from "./redux/Cart/Cart";
 import loginReducer from "./redux/login/login";
 import registrationReducer from "./redux/Registration/Registration";
 
@@ -16,3 +16,6 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(kinkApi.middleware),
 });
+
+
+store.dispatch(initializeCart());
